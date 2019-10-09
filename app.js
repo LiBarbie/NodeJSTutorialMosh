e0ef65451aac5ee1fea8c7228d75495cb41ba2a6
Linda Barbiero <linda.barbiero.st@everis.com>
2019-10-09 16:23:57 +0100

    Doing something I'm too tired to explain

---
+++ app.js
@@ -0,0 +1,7 @@
+const log = require('./logger');
+
+//console.log(logger);
+
+//logger.log("Hello World");
+
+log('message');
---
+++ logger.js
@@ -0,0 +1,14 @@
+var url = 'http://mylogger.io/log';
+
+function log(message){
+    //Send an HTTP Request
+    console.log(message);
+}//log
+
+//Exporting an object
+module.exports.log = log;
+
+//Exporting only a function
+module.exports = log;
+
+//module.exports.endPoint = url;
