96cd4ed59c2f2ba332ecac45e1d58a23aafd6022
Linda Barbiero <linda.barbiero.st@everis.com>
2019-10-09 16:45:53 +0100

    Tested FS Modules in App.js

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-const os = require('os');
+const fs = require('fs');
 
-var totalMemory = os.totalmem();
-var freeMemory = os.freemem();
+const files =  fs.readdirSync('./');
 
-console.log('Ttal Memory : '+totalMemory+'\nFree Memory : '+freeMemory+'\nUsed Memory : '+(totalMemory-freeMemory));
+console.log(files);
+
+fs.readdir('./', function(err,files){
+    if(err)throw err;
+    console.log(files);
+});
